340e6ba7acb30964103130ff52540cb445b9279f
Chad Lucas <chad.lucas.19@gmail.com>
2017-05-21 16:26:16 -0500

    add photo route

--- danebook/config/routes.rb
+++ danebook/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   get '/edit' => 'static_pages#edit'
   get '/friends' => 'static_pages#friends'
+  get '/photos' => 'static_pages#photos'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
